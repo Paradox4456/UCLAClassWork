d72d4e98bcfc0f74e5f5dbc6f6e1f51a1ef0357b
Paradox4456 <ethanmendoza@gmail.com>
2023-04-11 20:34:32 -0700

    1st Console.writeline Push

--- Assignments/Assignment 2.3/Program.cs
+++ Assignments/Assignment 2.3/Program.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 
-public class program()
+class Program
 {
-    
+    static void Main(string[] args)
+    {
+        int x = 10;
+        float y = 5.5f;
+        double z = 2.5;
+        int sum = x + (int)y;
+        Console.WriteLine($"x + y = {sum}");
+    }
+
 }
