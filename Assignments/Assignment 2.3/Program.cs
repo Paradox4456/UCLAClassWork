b776421a8d1912015f3eb7caf38a978171793660
Paradox4456 <ethanmendoza@gmail.com>
2023-04-17 08:41:01 -0700

    Completed 2.3 Assignment

--- Assignments/Assignment 2.3/Program.cs
+++ Assignments/Assignment 2.3/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 10;
+        double y = 3.14;
+        float z = 2.5f;
+
+        int sum = x + (int)y;
+        Console.WriteLine("x + y = " + sum);
+
+        float difference = (float)y - z;
+        Console.WriteLine("y - z = " + difference);
+
+        double product = x * z;
+        Console.WriteLine("x * z = " + product);
+
+        double quotient = x / z;
+        Console.WriteLine("x / z = " + quotient);
+    }
+}
