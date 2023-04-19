d148372b545b1f310560bd0fb3d196d5b4f0e386
Paradox4456 <ethanmendoza@gmail.com>
2023-04-19 10:45:07 -0700

    assignment 3.3

--- Assignments/ClassWS3.0/Program.cs
+++ Assignments/ClassWS3.0/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
 
-Console.WriteLine("Hello, World!");
+OpenAIAPI api = new OpenAIAPI("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");
+
+while (true)
+{
+    var chat = api.Chat.CreateConversation();
+    Console.WriteLine("");
+    var userInput = Console.ReadLine();
+    chat.AppendUserInput(userInput);
+
+    await foreach (var res in chat.StreamResponseEnumerableFromChatbotAsync())
+    {
+        Console.Write(res);
+    }
+
+    // the entire chat history is available in chat.Messages
+    foreach (ChatMessage msg in chat.Messages)
+    {
+        Console.WriteLine($"{msg.Role}: {msg.Content}");
+    }
+
+    Console.WriteLine("Do you want to ask another question? (y/n)");
+    var response = Console.ReadLine();
+    if (response.ToLower() != "y")
+    {
+        break;
+    }
+}
